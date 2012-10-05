a0dc298a87c351a3755aeb02816640c140d24093
Oscar <Oscar@172.17.9.3>
2012-10-04 18:50:02 -0600

    Commit inicial, colocando los archivos dados para realizar la tarea.

---
+++ src/OriginalProgram/WordSetFactory.java
@@ -0,0 +1,33 @@
+package OriginalProgram;
+
+/*
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7 
+Autor: Eduardo Castellanos
+
+Descripci�n: WordSetFactory. Clase  utilizada para instanciar los diferentes tipos de sets. 
+*/
+class WordSetFactory {
+	
+	// Metodo que genera un objeto que implementa WordSet
+	// parametro tipo: 1 = SimpleSet
+	//                         2 = implementado con Red black tree
+	//                         3 = implementado con Splay Tree
+	//                         4 = implementado con Hash Table
+	
+	public static WordSet generateSet(int tipo)
+	{
+	    if (tipo == 1)
+		    return new SimpleSet();
+		else
+		    // aqui se regresara el set empleando sus implementaciones:
+			// if tipo == 2 cree una instancia para un Wordset implementao con Red Black Tree
+			// if tipo == 3 cree una instancia para un Wordset implementado con Splay Tree
+			// if tipo == 4 cree una instancia para un Wordset impelementdo con Hash table
+			
+			return null;
+	}
+	
+	
+}
